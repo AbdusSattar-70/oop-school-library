9ac6a05a1ddafb0f2ce625e1dcda9a02dccacb11
Abdus Sattar <sasorganicagro@gmail.com>
2023-05-29 19:58:04 +0600

    Add person class

---
+++ person.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..500)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
+
+  public
+
+  def can_use_services?
+    return true if of_age? || @parent_permission == true
+
+    false
+  end
+end
