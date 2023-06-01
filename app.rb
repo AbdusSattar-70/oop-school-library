95045c7ac7f46c9f1169e747a3e7b9de16a932a8
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-02 00:10:09 +0600

    Add main method & functionality for interaction

--- app.rb
+++ app.rb
@@ -18,7 +18,11 @@ class App
   end
 
   def list_all_books
-    @books.each { |book| puts "Title: #{book.title} Author: #{book.author}" }
+    if @books.empty?
+      puts 'Book list is empty!'
+    else
+      @books.each { |book| puts "Title: #{book.title} Author: #{book.author}" }
+    end
   end
 
   def create_student
@@ -67,7 +71,11 @@ class App
   end
 
   def list_all_people
-    @people.each { |person| puts "ID: #{person.id} Name: #{person.name} Age: #{person.age}" }
+    if @people.empty?
+      puts 'People list is empty!'
+    else
+      @people.each { |person| puts "ID: #{person.id} Name: #{person.name} Age: #{person.age}" }
+    end
   end
 
   def create_rental
@@ -88,18 +96,22 @@ class App
   end
 
   def list_all_rentals
-    print 'ID of a person: '
-    id = gets.chomp.to_i
-
-    person_rental = @people.find { |person| person.id == id }
-
-    if person_rental
-      puts 'Rentals:'
-      person_rental.rentals.each do |rental|
-        puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
-      end
+    if @people.empty?
+      puts 'People list is empty!'
     else
-      puts 'Person not found!'
+      print 'ID of a person: '
+      id = gets.chomp.to_i
+
+      person_rental = @people.find { |person| person.id == id }
+
+      if person_rental
+        puts 'Rentals:'
+        person_rental.rentals.each do |rental|
+          puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
+        end
+      else
+        puts 'Person not found! May be ID is incorrect'
+      end
     end
   end
 end
