a35a23a224cac8c0f689ae3ff658d047ecd8de1b
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-08 15:53:01 +0600

    Add teacher class test

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../classes/teacher'
+describe Teacher do
+  before :each do
+    @teacher = Teacher.new(32, "specialization", 'Sattar', 452)
+  end
+it '#new or instance of Teacher class' do
+  expect(@teacher).to be_instance_of Teacher
+end
+
+it 'Has attributes following' do
+  expect(@teacher).to have_attributes(age: 32, specialization: 'specialization',name: 'Sattar', id: 452, parent_permission: true)
+end
+
+it 'Checks can_use_services? method' do
+  expect(@teacher.can_use_services?).to eql(true)
+end
+
+end
